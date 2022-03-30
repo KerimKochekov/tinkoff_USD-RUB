20cbcb38752dda61b4696c5b033788d5f1689c0e
Kerim Kochekov <50640842+KerimKochekov@users.noreply.github.com>
2022-03-30 16:19:45 +0300

    Add files via upload

---
+++ buy.py
@@ -0,0 +1,39 @@
+from numpy import double
+import requests
+import datetime
+import time
+
+def sd(x):
+	if x < 10:
+		return '0' + str(x)
+	return str(x)
+
+now = datetime.datetime.now()
+
+mn, mx = 150, 50
+
+with open("database/buy/" + str(now.year)+" - " + str(now.month) + "-" + str(now.day) + ".txt", "r+") as f:
+	for line in f:
+		val = double(line.split()[2])
+		mn = min(mn, val)
+		mx = max(mx, val)
+
+	while(1):
+
+		now = datetime.datetime.now()
+		request = requests.get("https://www.tinkoff.ru/api/v1/currency_rates/").json()
+
+		for rate in request['payload']['rates']:
+			if rate['fromCurrency']['name'] == 'USD' and rate['toCurrency']['name'] == 'RUB' and rate['category'] == 'PrepaidCardsTransfers':
+				
+				f.write("{}:{}:{} | {}\n".format(sd(now.hour), sd(now.minute), sd(now.second), rate['buy']))
+
+				mx = max(mx, double(rate['buy']))
+				mn = min(mn, double(rate['buy']))
+
+				print("{}:{}:{} | mn = {} | mx = {} | cur = {}".format(sd(now.hour), sd(now.minute), sd(now.second), mn, mx, rate['buy']))
+				
+				time.sleep(30)
+			
+	f.close()
+
